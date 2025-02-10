60ca03da8a88b0f5a7549d082fa1179497a2cd74
hzj04473 <hzj04473@naver.com>
2025-02-10 16:53:33 +0900

    0210. 회원정보 수정 / 비밀번호 컨펌

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -3,9 +3,9 @@ const User = require('../model/User');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-const userConroller = {};
+const userController = {};
 
-userConroller.createUser = async (req, res) => {
+userController.createUser = async (req, res) => {
   try {
     const { email, name, password } = req.body;
     const user = await User.findOne({ email });
@@ -26,7 +26,7 @@ userConroller.createUser = async (req, res) => {
   }
 };
 
-userConroller.loginWithEmail = async (req, res) => {
+userController.loginWithEmail = async (req, res) => {
   try {
     const { email, password } = req.body;
 
@@ -50,7 +50,7 @@ userConroller.loginWithEmail = async (req, res) => {
   }
 };
 
-userConroller.getUser = async (req, res) => {
+userController.getUser = async (req, res) => {
   try {
     const { userId } = req;
     // console.log(userId);
@@ -66,7 +66,7 @@ userConroller.getUser = async (req, res) => {
   }
 };
 
-userConroller.logout = (req, res) => {
+userController.logoutUser = (req, res) => {
   try {
     res.status(200).json({ status: 'success' });
   } catch (error) {
@@ -74,4 +74,79 @@ userConroller.logout = (req, res) => {
   }
 };
 
-module.exports = userConroller;
+// 회원정보 비밀번호 체크
+userController.editUserPasswordConfirm = async (req, res) => {
+  try {
+    const { email, passwordConfirm } = req.body;
+
+    const user = await User.findOne({ email }, ' -createdAt -updatedAt -__v');
+
+    if (user) {
+      const isMath = bcrypt.compareSync(passwordConfirm, user.password);
+      // console.log(isMath);
+      if (isMath) {
+        // console.log(isMath);
+        // res.send('비밀번호 체크 성공');
+        return res.status(200).json({ status: 'success', isMath });
+      } else {
+        throw new Error('회원의 비밀번호가 틀립니다.');
+      }
+    }
+    throw new Error('가입된 회원이 아닙니다.');
+  } catch (error) {
+    res.status(400).json({ status: 'fail', message: error.message });
+  }
+};
+
+// 회원정보 수정
+userController.editUser = async (req, res) => {
+  try {
+    const { name, password, isMath } = req.body;
+
+    if (isMath) {
+      const { userId } = req;
+
+      const salt = bcrypt.genSaltSync(saltRounds);
+      const hash = bcrypt.hashSync(password, salt);
+
+      const user = await User.findOne(
+        { _id: userId },
+        ' -createdAt -updatedAt -__v'
+      );
+
+      if (!user) {
+        throw new Error('사용자를 찾을 수 없습니다.');
+      }
+
+      // 비밀번호가 입력되지 않았을 경우, 이름만 업데이트
+      let updateUser = {};
+
+      // 새로운 비밀번호가 기존 비밀번호와 다른 경우에만 업데이트
+      const isSamePassword = await bcrypt.compare(password, user.password);
+      if (!password) {
+        updateUser = await User.updateOne(
+          { _id: userId },
+          { $set: { name: name } }
+        );
+      } else {
+        if (!isSamePassword) {
+          updateUser = await User.updateOne(
+            { _id: userId },
+            { $set: { name: name, password: hash } }
+          );
+        } else {
+          updateUser = await User.updateOne(
+            { _id: userId },
+            { $set: { name: name } }
+          );
+        }
+      }
+      res.status(200).json({ status: 'ok', data: updateUser }); //   // throw new Error('이미 가입이 된 유저 입니다.');
+    } else {
+      throw new Error('회원의 비밀번호 체크가 되지 않았습니다.');
+    }
+  } catch (error) {
+    res.status(400).json({ status: 'fail', message: error.message });
+  }
+};
+module.exports = userController;
--- routes/user.api.js
+++ routes/user.api.js
@@ -1,18 +1,27 @@
 const express = require('express');
 const router = express.Router();
-const userConroller = require('../controller/user.controller');
+const userController = require('../controller/user.controller');
 const authController = require('../controller/auth.controller');
 // 회원가입
-router.post('/', userConroller.createUser);
+router.post('/', userController.createUser);
 
 // 로그인
-router.post('/login', userConroller.loginWithEmail);
+router.post('/login', userController.loginWithEmail);
 
 // 토큰을 통해 유저 id -> 유저 내용 보내주기
 // userConroller.getUser 미들웨어
-router.get('/me', authController.authenticate, userConroller.getUser);
+router.get('/me', authController.authenticate, userController.getUser);
 
 // 로그아웃
-router.post('/logout', authController.authenticate, userConroller.logout);
+router.post('/logout', authController.authenticate, userController.logoutUser);
 
+// 회원수정
+router.post('/edit', authController.authenticate, userController.editUser);
+
+// 회원비밀번호 체크
+router.post(
+  '/passwordConfirm',
+  authController.authenticate,
+  userController.editUserPasswordConfirm
+);
 module.exports = router;
